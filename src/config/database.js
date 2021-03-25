0c582c74b307a31344b30fbede25835c70f7ef3e
Renato Cantarino <cast.carlosc.utic@sebrae.com.br>
2021-03-25 17:24:45 -0300

    morgan

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb+srv://renatocantarino:XXX@cantarinocluster.yejbg.mongodb.net/itensdb?retryWrites=true&w=majority';
+const url = 'mongodb+srv://renatocantarino:WCoxRL7v5YrxLrht@cantarinocluster.yejbg.mongodb.net/itensdb?retryWrites=true&w=majority';
 const mongoDB = process.env.MONGODB_URI || url;
 
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require("morgan")
+const PORT = 8000;
 require('./config/database')
 
 
-const itemRoute = require('./routes/item.route'); 
+const itemRoute = require('./routes/item.route');
 const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/item', itemRoute);
+app.use(morgan("combined"))
 
-const PORT = 8000;
+
 app.listen(PORT, () => {
     console.log(`Servidor em execução na porta => ${PORT}`);
 });
